802ae0b7a29ced7d68bbe70eaf251bb9de0de536
olga <olga.nikitina@parlamind.com>
2020-05-28 00:57:18 +0200

    by Wife: fixed a mistake

--- telebot/dialog/dialogmanager.py
+++ telebot/dialog/dialogmanager.py
@@ -1,9 +1,17 @@
 class DialogManager:
     def get_next_move(self, msg):
         goodbyes = {"bye", "goodbye", "farewell", "ciao", "see you", "until later", "talk to you later"}
-        if msg.endswith("start"):
+        msg_text = msg.get("text")
+        if msg_text.endswith("start"):
             return "start"
-        elif msg.lower() in set({v.lower() : v for v in goodbyes}.values()):
+        elif msg_text.lower() in set({v.lower() : v for v in goodbyes}.values()):
             return "end"
+        elif len(msg.get("mood")) > 0:
+            self.save_mood_to_db(msg.get("mood"))
+            return "next"
         else:
             return "next"
+
+
+    def save_mood_to_db(self, mood):
+        return
--- telebot/nlu/analyzer.py
+++ telebot/nlu/analyzer.py
@@ -1,8 +1,26 @@
 class NLU:
 
-    def parse(self, msg):
+    def parse(self, msg:str):
+        return {"text": self.preprocess(msg), "mood" : self.parse_mood(msg)}
+
+    def parse_mood(self, msg:str):
+        mood_markers = ["i'm", "i am", "i feel", "it feels like"]
+        is_mood = any([x in msg.casefold() for x in mood_markers])
+        if is_mood:
+            is_negated =  any([x in msg.casefold() for x in ["not", "no", "don't"]])
+            for feeling in ["positive", "negative", "neutral"]:
+                if (self.is_feeling(msg, feeling) and not is_negated):
+                    return feeling
+                elif is_negated and feeling == "positive":
+                    return "negative"
+                elif is_negated and feeling == "negative":
+                    return "positive"
+        return ""
+
+    def is_feeling(self, msg, feeling):
+        with open("/resources/"+feeling) as f:
+            result = [x.strip() for x in f.readlines()]
+        return any([x in msg.casefold() for x in result])
+
+    def preprocess(self, msg:str):
         return msg
-
-
-    def tokenize(self, msg):
-        return msg.replace(".", "").split()
