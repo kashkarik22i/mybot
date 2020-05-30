fbe39c352e3eadf7a40f2dba5be87b4b7b14bb59
olga <olga.nikitina@parlamind.com>
2020-05-30 22:09:58 +0200

    by Wife: added a unit test for dialogmanager

--- telebot/dialog/dialogmanager.py
+++ telebot/dialog/dialogmanager.py
@@ -1,10 +1,11 @@
 class DialogManager:
+    goodbyes = {"bye bye","bye", "goodbye", "farewell", "ciao","cu", "see u", "c u", "see you", "until later", "till later", "talk to you later"}
+
     def get_next_move(self, msg):
-        goodbyes = {"bye", "goodbye", "farewell", "ciao", "see you", "until later", "talk to you later"}
         msg_text = msg.get("text")
         if msg_text.endswith("start"):
             return "start"
-        elif msg_text.lower() in set({v.lower() : v for v in goodbyes}.values()):
+        elif msg_text.lower() in DialogManager.goodbyes:
             return "end"
         elif len(msg.get("mood")) > 0:
             self.save_mood_to_db(msg.get("mood"))
---
+++ test/unittest_dialogmanager.py
@@ -0,0 +1,21 @@
+import unittest
+from telebot.dialog.dialogmanager import DialogManager
+
+class test_dialogmanager(unittest.TestCase):
+
+    def test_get_next_move_goodbye(self):
+        test_msg = {"text" : "bye bye", "mood" : ""}
+        self.assertEqual(DialogManager().get_next_move(test_msg), "end")
+
+
+    def test_get_next_move_start(self):
+        test_msg = {"text" : "sssstart", "mood" : ""}
+        self.assertEqual(DialogManager().get_next_move(test_msg), "start")
+
+
+    def test_get_next_move_mood(self):
+        test_msg = {"text" : "I'm fine", "mood" : "positive"}
+        self.assertEqual(DialogManager().get_next_move(test_msg), "mood")
+
+if __name__ == '__main__':
+    unittest.main()
